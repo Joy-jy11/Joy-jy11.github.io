28a9ebd0f1d878ed11cbef3fa112018cc2d1e328
Joy-jy11 <76440770+Joy-jy11@users.noreply.github.com>
2025-04-12 16:43:32 -0400

    color change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: false,
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+	safelist: [
+		'bg-gradient-to-r',
+		'from-lime-400',
+		'via-zinc-300',
+		'to-rose-300',
+		'bg-clip-text',
+		'text-transparent',
+		'drop-shadow-[1px_1px_0_#e5e7eb]',
+	],
+
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Noto Sans'],
+			},
+			dropShadow: {
+				'soft-outline': '1px 1px 0 #e5e7eb',
+			},
+		},
 	},
-	plugins: [require('@tailwindcss/typography'),],
-	theme: {
-		fontFamily: {
-			'sans': ['Noto Sans']
-		}
-	}
+
+	plugins: [require('@tailwindcss/typography')],
 }
